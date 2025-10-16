e382f9a55585dd7ca989df98f34cbf87b8771e66
Dilon Sjamsuddin <130769530+wasdeilonn@users.noreply.github.com>
2025-10-16 19:00:46 +0200

    Update Data.cs

--- ButtonClicker2/ButtonClicker2/Data.cs
+++ ButtonClicker2/ButtonClicker2/Data.cs
@@ -18,7 +18,18 @@ namespace ButtonClicker2
 
         public enum UpgradeBank
         {
-            //fill out later
+            Koki, //high sell price, low produce
+            Baliko, //low sell price, high produce
+            AutoClicker, //medium sell price, medium produce. should scale with something
+            Dice6, //1/6 chance to get 20% the goal
+            Dice20, //1/20 chance to get 100% the goal
+            MegaKnight, //doubles click value, including autoclicker. also makes a stupid smash sound
+            Sandwich, //give 5% of the goal
+            TimeMachine, //removes 5 s of time
+            FileExplorer, //nonexistent sell price, high produce
+            RecycleBin, //doubles file explorer produce
+            Virus, //every click takes away 5% of the goal from your total, but it stores it in the sell value. every round end doubles the stored value
+            Miner, //pools produce in sell price, doubles if you have virus, halves with file explorer
         }
 
         public struct UpgradeData()
